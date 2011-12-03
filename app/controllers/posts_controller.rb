4bf34b1d21429925155e16852ecad96b0df45d60
Jonathan ponce <jpon91@yahoo.com>
2011-12-02 23:03:47 -0600

    maso

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,9 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
+ 
     @posts = Post.all
-
+	
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
