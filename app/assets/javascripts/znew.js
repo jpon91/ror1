8d447bd9fc353a4c370bf544796c48ca565ed6d6
Jonathan ponce <jpon91@yahoo.com>
2011-12-04 10:30:36 -0600

    iemalo

--- app/assets/javascripts/znew.js
+++ app/assets/javascripts/znew.js
@@ -51,34 +51,3 @@
 	}
 		}
 	}
-	curvyCorners.addEvent(window, 'load', initCorners);
-
-  function initCorners() {
-    var settings = {
-      tl: { radius: 5 },
-      tr: { radius: 15 },
-      bl: { radius: 5 },
-      br: { radius: 15 },
-      antiAlias: true
-    }
-
-    /*
-    Usage:
-
-    curvyCorners(settingsObj, selectorStr);
-    curvyCorners(settingsObj, Obj1[, Obj2[, Obj3[, . . . [, ObjN]]]]);
-
-    selectorStr ::= complexSelector [, complexSelector]...
-    complexSelector ::= singleSelector[ singleSelector]
-    singleSelector ::= idType | classType
-    idType ::= #id
-    classType ::= [tagName].className
-    tagName ::= div|p|form|blockquote|frameset // others may work
-    className : .name
-    selector examples:
-      #mydiv p.rounded
-      #mypara
-      .rounded
-    */
-    curvyCorners(settings, "#myBox");
-  }
